5b4bc708ea177523d69817a9946d35488711f448
Christophe Biocca <christophe.biocca@gmail.com>
2018-06-06 08:49:14 -0400

    Tried to port the code to not use tokio-process.
    I'm no longer able to recreate the issue.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
 extern crate futures;
-extern crate tokio_process;
+extern crate tokio_signal;
+extern crate libc;
 
 use std::io;
-use std::process::{Command,Stdio};
-use tokio_process::CommandExt;
-use futures::{Future,IntoFuture,Stream,stream};
+use std::time::Duration;
+use std::thread::{self,JoinHandle};
+use std::process::{Command,ExitStatus};
+use tokio_signal::unix::Signal;
+use futures::{Future,Stream};
 
 // Manually inlining the code at its call site also seems to fix the issue, which seems insane to me?
-fn run_command(idx: usize) -> impl Future<Item=bool, Error=io::Error> {
-    let child_status = Command::new("echo")
-        .arg(format!("I am spawned process #{}", idx))
-        // The folllowing lines can be uncommented and the bug will still occur.
-        //.stdin(Stdio::null())
-        //.stdout(Stdio::null())
-        //.stderr(Stdio::null())
-        .spawn_async();
-
-    child_status.into_future().flatten().map(move |s|{
-        println!("Result #{}: {:?}", idx, s);
-        s.success()
-    })
+fn run_command(idx: usize) -> JoinHandle<Result<ExitStatus, io::Error>> {
+    thread::spawn(
+        move ||{
+            let status = Command::new("echo")
+                .arg(format!("I am spawned process #{}", idx))
+                .status();
+            println!("Task {} finished with status {:?}", idx, status);
+            status
+        },
+    )
 }
 
 fn main() {
-    // Tweaking the total number of tasks doesn't fix the issue.
-    // It seems to always hang on the last task.
-    let check_stream = stream::iter_ok((0..2).into_iter()).map(|i|{
-        run_command(i)
-    // Higher levels of buffering still have the bug.
-    // Lowering to one fixes the issue (but that's equivalent to not using it).
-    // buffer_unordered behaves the same
-    }).buffered(2);
-    let results = check_stream.collect().wait();
-    println!("{:?}", results);
+    for i in 0..11 {
+        run_command(i);
+    }
+    let signal = Signal::new(libc::SIGCHLD);
+    for notif in signal.flatten_stream().wait() {
+        println!("Notified: {:?}", notif);
+    }
 }
